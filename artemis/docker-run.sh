095dc89de6a39ec77de358c74bae14a2999ec76c
MossOnTheTree <mossonthetree@gmail.com>
2024-08-03 18:12:27 +0200

    removed version from compose files

--- artemis/docker-run.sh
+++ artemis/docker-run.sh
@@ -1,23 +1,23 @@
-set -e
+# set -e
 
-BROKER_HOME=/var/lib/
-CONFIG_PATH=$BROKER_HOME/etc
-export BROKER_HOME OVERRIDE_PATH CONFIG_PATH
+# BROKER_HOME=/var/lib/
+# CONFIG_PATH=$BROKER_HOME/etc
+# export BROKER_HOME OVERRIDE_PATH CONFIG_PATH
 
-if [[ ${ANONYMOUS_LOGIN,,} == "true" ]]; then
-  LOGIN_OPTION="--allow-anonymous"
-else
-  LOGIN_OPTION="--require-login"
-fi
+# if [[ ${ANONYMOUS_LOGIN,,} == "true" ]]; then
+#   LOGIN_OPTION="--allow-anonymous"
+# else
+#   LOGIN_OPTION="--require-login"
+# fi
 
-CREATE_ARGUMENTS="--user ${ARTEMIS_USER} --password ${ARTEMIS_PASSWORD} --silent ${LOGIN_OPTION} ${EXTRA_ARGS}"
+# CREATE_ARGUMENTS="--user ${ARTEMIS_USER} --password ${ARTEMIS_PASSWORD} --silent ${LOGIN_OPTION} ${EXTRA_ARGS}"
 
-echo CREATE_ARGUMENTS=${CREATE_ARGUMENTS}
+# echo CREATE_ARGUMENTS=${CREATE_ARGUMENTS}
 
-if ! [ -f ./etc/broker.xml ]; then
-    /opt/activemq-artemis/bin/artemis create ${CREATE_ARGUMENTS} .
-else
-    echo "broker already created, ignoring creation"
-fi
+# if ! [ -f ./etc/broker.xml ]; then
+#     /opt/activemq-artemis/bin/artemis create ${CREATE_ARGUMENTS} .
+# else
+#     echo "broker already created, ignoring creation"
+# fi
 
-exec ./bin/artemis "$@"
+# exec ./bin/artemis "$@"
